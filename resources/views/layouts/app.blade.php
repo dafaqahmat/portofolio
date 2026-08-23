33a06f7b2e194af4210d2761b240eb83150dcd44
dafaqahmat <dafaqahmat57@gmail.com>
2026-08-23 07:02:41 +0700

    fix: navbar admin active untuk Projects

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,12 +25,12 @@
                         </div>
 
                         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <a href="{{ route('dashboard') }}" class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition">
-                                Dashboard
-                            </a>
-                            <a href="{{ route('admin.projects.index') }}" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition">
-                                Projects
-                            </a>
+                            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                                {{ __('Dashboard') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('admin.projects.index')" :active="request()->routeIs('admin.projects.*')">
+                                {{ __('Projects') }}
+                            </x-nav-link>
                         </div>
                     </div>
 
